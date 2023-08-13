3e0b5b79ddaa7dea7b3c6a74c196ded967261f0e
shrikantkamle <52593964+shrikantkamle@users.noreply.github.com>
2023-08-13 13:23:59 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -7,6 +7,6 @@ API.init_app(app)
 app.config["JWT_SECRET_KEY"] = 't1NP63m4wnBg6nyHYKfmc2TpCOGI4nss'
 jwt = JWTManager(app)
 app.config['PROPAGATE_EXCEPTIONS'] = True
-
+print("flask started..............")
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
