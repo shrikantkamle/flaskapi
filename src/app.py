e7695984f3845721953604aba7e70fd1775a5605
shrikant <kamle.shrikant@gmail.com>
2023-08-16 20:03:21 +0530

    minor changes

--- src/app.py
+++ src/app.py
@@ -3,6 +3,8 @@ from controller import API
 from flask_jwt_extended import JWTManager
 from settings import my_password
 import os
+
+
 app = Flask(__name__)
 API.init_app(app)
 app.config["JWT_SECRET_KEY"] = 't1NP63m4wnBg6nyHYKfmc2TpCOGI4nss'
@@ -25,14 +27,14 @@ def before_request_callback():
         print("result----",result)
     print("latest password is :",os.environ.get("my_password"))    
 
-@app.before_request
-def update_env():
-    print("before request----------")
-    if my_password == 'nopassword':
-        print("updating latest password")
-        result = os.system(f"export my_password={'SHIRKANT'}")
-        print("result----",result)
-    print("latest password is :",os.environ.get("my_password"))    
+# @app.before_request
+# def update_env():
+#     print("before request----------")
+#     if my_password == 'nopassword':
+#         print("updating latest password")
+#         result = os.system(f"export my_password={'SHIRKANT'}")
+#         print("result----",result)
+#     print("latest password is :",os.environ.get("my_password"))    
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
