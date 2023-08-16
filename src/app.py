426634cb2ccf5d5aff4d1acaab421a0c33227904
shrikant <kamle.shrikant@gmail.com>
2023-08-16 19:00:58 +0530

    minor changes

--- src/app.py
+++ src/app.py
@@ -2,11 +2,19 @@ from flask import Flask
 from controller import API
 from flask_jwt_extended import JWTManager
 from settings import my_password
+import os
 app = Flask(__name__)
 API.init_app(app)
 app.config["JWT_SECRET_KEY"] = 't1NP63m4wnBg6nyHYKfmc2TpCOGI4nss'
 jwt = JWTManager(app)
 app.config['PROPAGATE_EXCEPTIONS'] = True
 print("flask started..............",my_password)
+
+@app.before_request()
+def update_env():
+    if my_password == 'nopassword':
+        os.system(f"export my_password={'SHIRKANT'}")
+    print("latest password is :",os.environ.get("my_password"))    
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
--- src/settings.py
+++ src/settings.py
@@ -1,2 +1,5 @@
 import os
 my_password=os.environ.get("my_password","nopassword")
+
+
+print("shrikant",my_password)
