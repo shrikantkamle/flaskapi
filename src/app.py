ca1611193ce8ef16dca288b25709d3690a71a4f7
shrikant <kamle.shrikant@gmail.com>
2023-08-16 21:01:38 +0530

    minor changes 1

--- src/app.py
+++ src/app.py
@@ -3,7 +3,11 @@ from controller import API
 from flask_jwt_extended import JWTManager
 from settings import my_password
 import os
+import logging as logger
+import logging
 
+logging.basicConfig(level=logging.DEBUG)
+logging.debug('This will get logged')
 
 app = Flask(__name__)
 API.init_app(app)
@@ -17,15 +21,20 @@ print("flask started 1..............",my_password)
 def before_request_callback():   
     path = request.path   
     method = request.method   
-   
+    logger.info("sssssssssssssssssssssss")
     print("path and request is ====", path + " [" + method + "]" )   
     
     print("before request----------")
+    logger.info("before request----------")
     if my_password == 'nopassword':
+        logger.info("updating latest password")
         print("updating latest password")
-        result = os.system(f"export my_password={'SHIRKANT'}")
-        print("result----",result)
-    print("latest password is :",os.environ.get("my_password"))    
+        result = os.system(f"export my_password={'HELLOWORLD'}")
+        # result = os.system(f"SETX /m my_password {'SHIRKANT'}")
+        logger.info(f"result---- {result}")
+    new_password = os.environ.get("my_password")    
+    logger.info(f"latest password is :")    
+    logger.info(f"latest password is :{new_password}")    
 
 # @app.before_request
 # def update_env():
@@ -37,4 +46,4 @@ def before_request_callback():
 #     print("latest password is :",os.environ.get("my_password"))    
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0')
